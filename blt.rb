89ba32133dcfab63076e6569a0f11a5cb84c4560
Jeremy Yun <jeremy@athena.local>
2011-01-04 06:48:57 -0800

    added erb templating

--- blt.rb
+++ blt.rb
@@ -4,25 +4,14 @@ require 'sinatra'
 require 'twitter'
 require 'erb'
 
+HASHTAG = "c5"
+
 #Initialize a Twitter search client
 search = Twitter::Search.new
-stream = search.hashtag("bootylog").per_page(10).fetch.each {|p| puts p}
 
-page = <<PAGE
-  <div class="header">header</div>
-  <div class="body">
-    <div class="instructions">instructions</div>
-    <div class="stream">
-PAGE
 
-search.hashtag("bootylog").per_page(10).fetch.each {|p| page += p.text}
-
-page += <<PAGE_END
-    </div>
-  </div>
-  <div class="footer">footer</div>
-PAGE_END
- 
 get '/' do
-  page
+  @stream = ""
+  search.hashtag(HASHTAG).per_page(10).fetch.each {|p| @stream << "<li>#{p.text}</li>"}
+  erb :home
 end
