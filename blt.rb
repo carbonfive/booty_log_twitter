eb2e22cd0394f6eab0fbe7b1b4cdb9459c9532b2
Jeremy Yun <jeremy@carbonfive.com>
2011-01-12 11:30:44 -0800

    persist twitter feed to db and render page from db data

--- blt.rb
+++ blt.rb
@@ -6,14 +6,15 @@ require 'erb'
 require 'sequel'
 
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blt.db')
-# DB = Sequel.sqlite('./blt.db')
 DB.create_table :tweets do
   primary_key :id
   Long :t_id
   String :t_user
   String :t_text
+  Datetime :t_datetime
 end
 
+DEFAULT_REFRESH = 10000
 HASHTAG     = "bootylog"
 USER_COLORS = ['#f6b6d2',
                '#5cbb69',
@@ -53,27 +54,33 @@ helpers do
         "a long time ago"
     end
   end
-
+  
   def render_page(params)
     search  = Twitter::Search.new
     @stream = ""
     @hashtag = params[:h] || HASHTAG
-    @timeout = params[:t] || 10000
+    @timeout = params[:t] || DEFAULT_REFRESH
     @count = params[:c] || 20
     tweets = DB[:tweets]
+    last_id = tweets.order(:t_id).last ? tweets.order(:t_id).last[:t_id] : 0
 
-    search.hashtag(@hashtag).per_page(@count).fetch.each_with_index do |p, idx|
+    search.hashtag(@hashtag).since_id(last_id).fetch.each do |p|
       begin
-        tweets.insert(:t_id => p.to_json, :t_user => p.from_user, :t_text => p.text)
-        msg = p.text
-        regexes = Regexp.union(/^#bootylog/i, /#bootylog$/i)
-        msg.gsub!(regexes, '')
-        user_color = USER_COLORS[p.from_user.hash % USER_COLORS.size]
-        time_ago   = time_ago_in_words(Time.parse(p.created_at))
-        @stream << "<li><span class='user' style='color:#{user_color}'>#{p.from_user}</span>#{msg}&nbsp;&nbsp;<span class='time'>#{time_ago}</span></li>"
+        # update the local store if it has not been updated within the last 10 seconds
+        tweets.find
+        tweets.insert(:t_id => p.id, :t_user => p.from_user, :t_text => p.text, :t_datetime => p.created_at)
       rescue
         # ignore any tweets that cause errors
       end
+    end
+
+    tweets.each do |p|
+        msg = p[:t_text]
+        regexes = Regexp.union(/^#bootylog/i, /#bootylog$/i)
+        msg.gsub!(regexes, '')
+        user_color = USER_COLORS[p[:t_user].hash % USER_COLORS.size]
+        time_ago   = time_ago_in_words(p[:t_datetime])
+        @stream << "<li><span class='user' style='color:#{user_color}'>#{p[:t_user]}</span>#{msg}&nbsp;&nbsp;<span class='time'>#{time_ago}</span></li>"
     end
     erb :home
   end
